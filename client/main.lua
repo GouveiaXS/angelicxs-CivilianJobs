f134eef0a0723fc7659e1a19498fdf982821218d
AngelicXS <99211701+GouveiaXS@users.noreply.github.com>
2023-06-25 09:02:05 -0600

    Update main.lua
    Removed old code line

--- client/main.lua
+++ client/main.lua
@@ -233,7 +233,6 @@ RegisterNetEvent('angelicxs-CivilianJobs:MAIN:RouteMarker', function(table, rout
             local coord = GetEntityCoords(PlayerPedId())
 	    local safeCoord = vector3(route.x, route.y, route.z)
             local dist = #(coord-safeCoord)
-            local dist = #(coord-route)
             if dist <= 75 then
                 sleep = 550
                 if dist <= 25 then
